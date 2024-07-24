a387ffaa5a0c1ac6b468128a9df3cb569ca48cfa
Andreas Bagus Widodo <andreasbaguswidodo@gmail.com>
2024-07-24 23:32:15 +0800

    Page Info

--- application/controllers/frontend/Eventinfo.php
+++ application/controllers/frontend/Eventinfo.php
@@ -7,22 +7,16 @@ class Eventinfo extends CI_Controller {
 
     function index()
     {
+      $this->load->helper('common');
       $this->load->model("modelfrontend");
       $dataHeader = $this->modelfrontend->getDataHeader();
+      // $dataHeader->agent_open_date = mysqltodate($dataHeader->agent_open_date);
+      // $dataHeader->agent_close_date = mysqltodate($dataHeader->agent_close_date);
       $this->load->view('viewfrontend/layout/header', $dataHeader);
       $this->load->view('viewfrontend/layout/leftmenu', ['showback' => false, 'showmainmenu' => true, 'showadditionalmenu' => false]);
       $this->load->view('viewfrontend/eventinfo', $dataHeader);
       $this->load->view('viewfrontend/layout/rightmenu', ['showmainmenu' => true]);
       $this->load->view('viewfrontend/layout/footer', ['ajaxfilename'=> 'ajaxcollabolators.js']);
     }
-
-    function detail()
-    {
-      $collaboratorid = $this->input->post("collaboratorid");
-
-      $this->load->model("modelcollabolators");
-      $row = $this->modelcollabolators->getDetailcollabolators($collaboratorid);
-      echo json_encode($row);
-    }
-  
+ 
 }
--- application/views/viewfrontend/eventinfo.php
+++ application/views/viewfrontend/eventinfo.php
@@ -1,12 +1,26 @@
-<div class="card main-content bg-primary">
-    <div class="card-body">
+<div class="card main-content bg-transparent">
+    <div class="card-body text-light">
         <div class="row">
             <div class="col-5">
                 <h3>Info</h3>
                 <p><strong>Contact</strong></p>
-                <p><?php echo '';?></p>
+                <p><?php echo $event->contact_phone;?><br>
+                <?php echo $event->contact_email;?></p>
+                <a href="http://wa.me/<?php echo $event->contact_phone;?>" target="_blank" type="button" class="btn btn-secondary">Hubungi Kami</a>
             </div>
             <div class="col-7">
+                <p><strong>Lokasi Agen</strong></p>
+                <p>
+                    <?php echo $event->agent_open_date;?> - <?php echo $event->agent_close_date;?>
+                    <br>
+                    <?php echo $event->agent_open_time;?> - <?php echo $event->agent_close_time;?>
+                </p>
+                <p>
+                    <?php echo $event->agent_address;?>
+                </p>
+                <div class="iframe-rwd">
+                    <?php echo $event->agent_gmap;?>
+                </div>
             </div>
         </div>
     </div>
