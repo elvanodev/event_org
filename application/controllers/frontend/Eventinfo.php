7bdc8c85d54a2e643addf9e02a0904e135ab0dd2
Andreas Bagus Widodo <andreasbaguswidodo@gmail.com>
2024-07-25 18:42:01 +0800

    Event info FAQs

--- application/controllers/frontend/Eventinfo.php
+++ application/controllers/frontend/Eventinfo.php
@@ -8,13 +8,26 @@ class Eventinfo extends CI_Controller {
     function index()
     {
       $this->load->helper('common');
+      
+      $this->load->model("modelevents");
+      $row_event = $this->modelevents->getActiveEvent();
+      $event_id = $row_event->idx;
+
+      $this->load->model("modelfaq");
+      $list_faqs = $this->modelfaq->getListfaqByEvent($event_id);
+
       $this->load->model("modelfrontend");
       $dataHeader = $this->modelfrontend->getDataHeader();
-      // $dataHeader->agent_open_date = mysqltodate($dataHeader->agent_open_date);
-      // $dataHeader->agent_close_date = mysqltodate($dataHeader->agent_close_date);
+      $row_event->agent_open_date = mysqltodate($row_event->agent_open_date);
+      $row_event->agent_close_date = mysqltodate($row_event->agent_close_date);
+      $row_event->agent_open_time = substr($row_event->agent_open_time, 0, 5);
+      $row_event->agent_close_time = substr($row_event->agent_close_time, 0, 5);
+
+      $data = ['event'=>$row_event, 'list_faqs'=>$list_faqs];   
+
       $this->load->view('viewfrontend/layout/header', $dataHeader);
       $this->load->view('viewfrontend/layout/leftmenu', ['showback' => false, 'showmainmenu' => true, 'showadditionalmenu' => false]);
-      $this->load->view('viewfrontend/eventinfo', $dataHeader);
+      $this->load->view('viewfrontend/eventinfo', $data);
       $this->load->view('viewfrontend/layout/rightmenu', ['showmainmenu' => true]);
       $this->load->view('viewfrontend/layout/footer', ['ajaxfilename'=> 'ajaxcollabolators.js']);
     }
--- application/views/viewfrontend/eventinfo.php
+++ application/views/viewfrontend/eventinfo.php
@@ -4,24 +4,43 @@
             <div class="col-5">
                 <h3>Info</h3>
                 <p><strong>Contact</strong></p>
-                <p><?php echo $event->contact_phone;?><br>
-                <?php echo $event->contact_email;?></p>
-                <a href="http://wa.me/<?php echo $event->contact_phone;?>" target="_blank" type="button" class="btn btn-secondary">Hubungi Kami</a>
+                <p><?php echo $event->contact_phone; ?><br>
+                    <?php echo $event->contact_email; ?></p>
+                <a href="http://wa.me/<?php echo $event->contact_phone; ?>" target="_blank" type="button" class="btn btn-secondary">Hubungi Kami</a>
             </div>
             <div class="col-7">
                 <p><strong>Lokasi Agen</strong></p>
                 <p>
-                    <?php echo $event->agent_open_date;?> - <?php echo $event->agent_close_date;?>
+                    <?php echo $event->agent_open_date; ?> - <?php echo $event->agent_close_date; ?>
                     <br>
-                    <?php echo $event->agent_open_time;?> - <?php echo $event->agent_close_time;?>
+                    <?php echo $event->agent_open_time; ?> - <?php echo $event->agent_close_time; ?>
                 </p>
                 <p>
-                    <?php echo $event->agent_address;?>
+                    <?php echo $event->agent_address; ?>
                 </p>
                 <div class="iframe-rwd">
-                    <?php echo $event->agent_gmap;?>
+                    <?php echo $event->agent_gmap; ?>
                 </div>
             </div>
+        </div>
+        <h3>FAQs</h3>
+        <div class="accordion" id="accordionFaqs">            
+            <?php foreach ($list_faqs as $row) {
+            ?>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $row->idx; ?>" aria-expanded="false" aria-controls="collapse<?php echo $row->idx; ?>">
+                        <?php echo $row->faq_title; ?>
+                    </button>
+                </h2>
+                <div id="collapse<?php echo $row->idx; ?>" class="accordion-collapse collapse" data-bs-parent="#accordionFaqs">
+                    <div class="accordion-body">
+                        <?php echo $row->faq_detail; ?>
+                    </div>
+                </div>
+            </div>
+            <?php
+            } ?>
         </div>
     </div>
 </div>
